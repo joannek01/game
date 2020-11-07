da95c72effc5f2af6959073e25b38c44fbbb7db8
joanne kim <joannek0103@gmail.com>
2020-11-06 21:49:22 -0500

    initial work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import Two from "../assets/two.min.js";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ngOnInit(): void {
+  let elem = document.getElementById('draw-shapes');
+  let params = {fullscreen: true};
+  let two = new Two(params).appendTo(elem);
+
+  let circle = two.makeCircle(70,50,20);
+  circle.fill ='#FF8000';
+  let rect = two.makeRectangle(20,50,50,20);
+  two.update();
+}
   title = 'game';
 }
