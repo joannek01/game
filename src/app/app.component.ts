a3c15fee8f0483be969af8f5b4b622bcaf72ea05
joanne kim <joannek0103@gmail.com>
2020-11-20 21:38:02 -0500

    11/20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,42 +11,29 @@ ngOnInit(): void {
   let params = {fullscreen: true};
   let two = new Two(params).appendTo(elem);
 
-  let circle = two.makeCircle(70,50,20);
-  circle.fill ='#FF8000';
-  let x=70
-  let y=50
-  let circle2 = two.makeCircle(30,20,10);
-  circle2.fill = '#22FFAA';
-  let circle3 = two.makeCircle(10,30,10);
-  circle3.fill = '#BB1122'
-  let circle4 = two.makeCircle(1000,40,30);
-  circle4.fill = '#FFFF00';
-  let circle5= two.makeCircle(700,100,100);
-  circle5.fill = '#1111FF';
+  //let rectangle = two.makeRectangle(300,200,70,30)
+  
+  let circle = two.makeCircle(200,70,50)
 
-  let rect = two.makeRectangle(20,50,50,20);
-  rect.fill = '#BBAAFF';
-  let rect2 = two.makeRectangle(40,90,20,50);
-  rect2.fill = '#FFCCCC';
-  let rect3 = two.makeRectangle(70,100,20,20);
-  rect3.fill = '#00FFFF';
-  let rect4 = two.makeRectangle(150,110,90,100);
-  rect4.fill = '#00AAFF';
-  let rect5 = two.makeRectangle(300,300,300,500);
-  rect5.fill = '#02AA01';
-  rect5.opacity = 0.50;
-  rect5.stroke = '#FFAACC'; 
-  rect5.linewidth = 5;
-
-  let ellipse = two.makeEllipse(500,200,50,30)
-  ellipse.fill = "#FF00CC"
-  let star = two.makeStar(550,300,30,70,5)
+  //let ellipse = two.makeEllipse(500,200,50,30)
+  //ellipse.fill = "#FF00CC"
+  //let star = two.makeStar(550,300,30,70,5)
+  
+  let scaleDelta = 0.1
 
   two.bind('update', (framesPerSecond)=>{
-    circle.translation.set(x,y);
-    x=x+2;
-    y=y+1;
+    //let changeintime = Date.now()/1000;
+    circle.scale = circle.scale+scaleDelta
+    if (circle.scale > 5) {
+      scaleDelta = -.01
+    }
+    if (circle.scale < 0.5) {
+      scaleDelta = .01
+    }
+    //rectangle.rotation = 5*changeintime%Math.PI;
+    //ellipse.rotation = 10*changeintime%Math.PI;
   }).play();
+
 
 }
   title = 'game';
