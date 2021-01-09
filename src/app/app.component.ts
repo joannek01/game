2e637d378dae5c3b9b078a8e3cf50b6c27014243
joanne kim <joannek0103@gmail.com>
2021-01-08 21:49:11 -0500

    1/8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,67 +1,89 @@
-import { Component, HostListener, OnInit } from '@angular/core';
-import Two from "../assets/two.min.js";
+import { HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Two from '../assets/two.min.js';
+import { CameraService } from './services/camera.service.js';
 import { Sprite, SpriteService } from './services/sprite.service.js';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
+  direction:string;
+  
   x: number=200;
   y: number=200;
-  
-  constructor(private _spriteService: SpriteService){}
+
+  max_x: number= 3500;
+  max_y: number= 2500;
+
+  constructor(private _spriteService: SpriteService, private _cameraService: CameraService) {}
 
   @HostListener('document:keydown', ['$event'])
-  handlekey(event: KeyboardEvent) {
+  handleKey(event: any) {
     if (event.key=='ArrowRight') {
       this.x=this.x+10;
-      this._spriteService.sprites[0].direction='right';
+      this._spriteService.sprites[0].direction='right'
     }
     else if (event.key=='ArrowLeft') {
       this.x=this.x-10;
       this._spriteService.sprites[0].direction='left';
     }
-    else if (event.key=='ArrowUp') {
+    else if (event.key =='ArrowUp') {
       this.y=this.y-10;
     }
     else if (event.key=='ArrowDown') {
-      this.y=this.y+10
+      this.y=this.y+10;
     }
+    event.preventDefault();
   }
 
-ngOnInit(): void {
-  let elem = document.getElementById('draw-shapes');
-  let params = {fullscreen: true};
-  let two = new Two(params).appendTo(elem);
+  ngOnInit(): void {
+    let elem = document.getElementById('map');
+    let params = {
+      width: this.max_x+'px',
+      height: this.max_y+'px'
 
-  for (let i=0; i<this._spriteService.sprites.length; i++) {
-    //console.log(this._spriteService.sprites[i]);
-    let sprite=this._spriteService.sprites[i];
-    this._spriteService.sprites[i].spriteReference=two.makeSprite(sprite.url, sprite.x, sprite.y, sprite.columns, sprite.rows, sprite.fps);
-    this._spriteService.sprites[i].spriteReference.play(this._spriteService.sprites[i].rightFrames[0], this._spriteService.sprites[i].rightFrames[1]);
-    //this._spriteService.sprites[i].spriteReference.scale=.5;
+    };
+    let two = new Two(params).appendTo(elem);
+
+    this._cameraService.init(this.max_x,this.max_y);
+
+    //loop through service
+    for (let i=0; i<this._spriteService.sprites.length; i++) {
+      let sprite=this._spriteService.sprites[i];
+      this._spriteService.sprites[i].spriteReference=two.makeSprite(sprite.url, sprite.x, sprite.y, sprite.columns, sprite.rows, sprite.fps);
+      this._spriteService.sprites[i].spriteReference.play(this._spriteService.sprites[i].rightFrames[0], this._spriteService.sprites[i].rightFrames[1]);
+      this._spriteService.sprites[i].spriteReference.scale=this._spriteService.sprites[i].scale;
+    }
+    //rectangle.scale=.7;
+    two.bind('update', (framesPerSecond)=>{
+      // this is where animatoin happens
+
+
+      if (this.x+70<this.max_x && this.x-70>0) this._spriteService.sprites[0].spriteReference.translation.x=this.x;
+      if (this.y+30<this.max_y && this.y-70>0) this._spriteService.sprites[0].spriteReference.translation.y=this.y;
+
+      this._cameraService.zoomCamera(this.x, this.y);
+      
+
+        for (let i=0; i<this._spriteService.sprites.length; i++) {
+          if (this._spriteService.sprites[i].direction != this._spriteService.sprites[i].lastDirection) {
+            this._spriteService.sprites[i].lastDirection=this._spriteService.sprites[i].direction;
+            if (this._spriteService.sprites[i].direction=='right') {
+              this._spriteService.sprites[i].spriteReference.play(this._spriteService.sprites[i].rightFrames[0], this._spriteService.sprites[i].rightFrames[1])
+            }
+            else {
+              this._spriteService.sprites[i].spriteReference.play(this._spriteService.sprites[i].leftFrames[0], this._spriteService.sprites[i].leftFrames[1])
+            }
+          }
+        }
+        
+    }).play();
   }
 
-  //rectangle.scale=.7;
-  two.bind('update', (framesPerSecond)=>{
-    // this is where animatoin happens
-    this._spriteService.sprites[0].spriteReference.translation.x=this.x;
-    this._spriteService.sprites[0].spriteReference.translation.y=this.y;
+  title = 'BabyShark';
 
-    for(let i=0; i<this._spriteService.sprites.length; i++) {
-      if (this._spriteService.sprites[i].direction !=this._spriteService.sprites[i].lastDirection) {
-        this._spriteService.sprites[i].lastDirection=this._spriteService.sprites[i].direction;
-        if (this._spriteService.sprites[i].direction=='right')
-          this._spriteService.sprites[i].spriteReference.play(this._spriteService.sprites[i].rightFrames[0], this._spriteService.sprites[i].rightFrames[1])
-        }
-        else {
-          this._spriteService.sprites[i].spriteReference.play(this._spriteService.sprites[i].leftFrames[0], this._spriteService.sprites[i].leftFrames[1])
-        }
-      }
-  }).play(); 
 
-}
-  //title = 'game';
 }
---
+++ src/app/services/camera.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CameraService {
+  max_x: number = 0;
+  max_y: number = 0;
+
+  diff_x: number = 320;
+  diff_y: number = 150;
+
+  constructor() { }
+  
+  init(x: number, y: number) {
+    this.max_x = x;
+    this.max_y = y;
+  }
+  zoomCamera(x: number, y: number){
+    if(this.max_x ==0) {
+      return;
+    }
+    if (this.max_y ==0) {
+      return;
+    }
+    let screen_x=x-this.diff_x;
+    let screen_y=y-this.diff_y;
+
+    window.scrollTo(screen_x, screen_y);
+  }
+}
